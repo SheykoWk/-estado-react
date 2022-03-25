f0d5374ba2638b11a50e5ae95cc772a32507b18e
SheykoWk <sheyko.wk@gmail.com>
2022-03-24 21:35:59 -0600

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [ count, setCount ]  = useState(0)
+
+  const minLimit = () => {
+    if(count > 0){
+      setCount(count - 1)
+    } 
+    return
+  }
+  const maxLimit = () => {
+    if(count < 0){
+      setCount(count + 1)
+    } 
+    return
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {count}
+        <button onClick={maxLimit}>Aumentar</button>
+        <button onClick={minLimit}>Disminuir</button>
       </header>
     </div>
   );
